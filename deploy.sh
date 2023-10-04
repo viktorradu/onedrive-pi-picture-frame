387d2b74a9dfebad02d2173ea4af468e3cf4023e
viktorradu <v.radu@outlook.com>
2023-10-04 15:28:16 -0400

    initial

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+rsync -av --exclude=.git ~/onedrive-pi-picture-frame wall1@192.168.107.81:/home/wall1
---
+++ frame.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+INTERVAL_SECONDS=5
+
+fbi -a -noverbose -blend 200 -t 10 "cache/proto.jpeg" "cache/proto2.jpg"
